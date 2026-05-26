08ab46478991dd86918bf541a35081ece1c8cd63
hqyone <hqyone@hqyonedeMacBook-Air.local>
2026-05-26 09:34:57 +0800

    style: fix MessageList.tsx formatting for prettier

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -200,7 +200,11 @@ export default function MessageList({
                 {item.type === "message" && item.msg && (
                   <MessageItem
                     msg={item.msg}
-                    isMe={item.msg.sender_name === myName || item.msg.sender_name === members.find((m) => m.is_me)?.name}
+                    isMe={
+                      item.msg.sender_name === myName ||
+                      item.msg.sender_name ===
+                        members.find((m) => m.is_me)?.name
+                    }
                     myName={myName || members.find((m) => m.is_me)?.name || ""}
                     members={members}
                     editingId={editingId}
